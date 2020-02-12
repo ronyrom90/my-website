4773e6601bb9fdbc54c9de6b6d0dee355f4db497
Rony Rom <ronyrom90@gmail.com>
2020-02-12 10:16:21 +0100

    r

--- script.js
+++ script.js
@@ -4,21 +4,3 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-/*Scroll to top when arrow up clicked BEGIN*/
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('.top').fadeIn();
-    } else {
-        $('.top').fadeOut();
-    }
-});
-$(document).ready(function() {
-    $(".top").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
-
-});
- /*Scroll to top when arrow up clicked END*/
