25dcc21f49ede88c4862b713b05347ed33d17307
Rony Rom <ronyrom90@gmail.com>
2020-04-11 08:51:15 +0200

    smoothscroll

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const link = document.getElementById("clickToUp");
-link.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(link.href);
-  // const goal = link.href
-  const goal = document.querySelector(link.getAttribute("href"));
-  console.log(goal);
-  goal.scrollIntoView({ behavior: "smooth" });
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
 });
