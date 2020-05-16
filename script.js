6ecbb1d60349d756fe56d5b75a9a8ccaa7433027
Rony Rom <ronyrom90@gmail.com>
2020-05-16 08:55:46 +0200

    loader

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ $(document).ready(function () {
     } // End if
   });
 });
+
+jQuery(document).ready(function ($) {
+  // site preloader -- also uncomment the div in the header and the css style           for #preloader
+  $(window).load(function () {
+    $(".preloader").fadeOut("slow");
+  });
+});
