9ee81159d3c5bce8ead07b2e73c9b39b9a7458e9
Rony Rom <ronyrom90@gmail.com>
2020-05-16 15:25:47 +0200

    loader

--- script.js
+++ script.js
@@ -28,6 +28,6 @@ $(document).ready(function () {
 jQuery(document).ready(function ($) {
   // site preloader -- also uncomment the div in the header and the css style           for #preloader
   $(window).load(function () {
-    $(".preloader").delay("350").fadeOut("slow");
+    $(".preloader").delay("150").fadeOut("slow");
   });
 });
